4ae0ca5d4700872dc16f1e5aab558ae1732e5d11
vipul-bhatia <vipul0592bhatia@gmail.com>
2024-02-22 02:05:24 +0530

    .

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -31,6 +31,19 @@ const Hero = (props) => {
         <div  className={`${styles.titleContainer} ${textVisible ? styles.visible : ''}`}>
           <img src="/tittlebannertextpngwithshadow.png" alt="Title Image" />
         </div>
+        <div className={styles.mobileImageContainer}>
+  <div className={`${styles.titleContainer1} ${textVisible ? styles.visible : ''}`}>
+    <img src="/1mobile.jpg" alt="Mobile Image 1" />
+  </div>
+  <div className={`${styles.titleContainer3} ${textVisible ? styles.animate : ''}`}>
+    <img src="/3mobile.png" alt="Mobile Image 3" />
+  </div>
+  <div className={`${styles.titleContainer2} ${textVisible ? styles.animate : ''}`}>
+    <img src="/2mobile.png" alt="Mobile Image 2" />
+</div>
+
+</div>
+
         {subtitle && <span className={styles.subtitle}>{subtitle}</span>}
         {ctaText && (
           <Button
